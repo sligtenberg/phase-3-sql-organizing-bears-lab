5841702edfd718482eeb4c5bb1714be99b289892
stephen ligtenberg <bountyhunter24@gmail.com>
2023-02-17 13:26:31 -0500

    commit tree 912f93d0320456f68771ad90ee152d5b60f678ac

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 23, 'M', 'red', 'happy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'blue', 'Stoked', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'purple', 'Jolly', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergent Brown', 3, 'M', 'black', 'Merry', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 15, 'F', 'green', 'Dull', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'green', 'Grouchy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 17, 'F', 'red', 'Silly', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 18, 'M', 'white', '???', 0);
